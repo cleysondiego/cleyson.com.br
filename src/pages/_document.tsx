c5b092b4201f889a67116d557db0368ae88430e3
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-05-17 19:23:35 -0300

    adding google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,17 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+            `
+          }} />
         </Head>
         <body>
           <Main />
