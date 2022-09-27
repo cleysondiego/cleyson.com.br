e10c31536d828bf25955523a4d849e28b4eca9d3
Cleyson Silva <cdsilva1@topaz.com.br>
2022-09-26 21:02:12 -0300

    feat: using next/image to show the image and add new tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
